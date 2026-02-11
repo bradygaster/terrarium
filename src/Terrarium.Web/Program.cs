9e4af40c019a19811691707bc680e15081f21f29
Brady Gaster <41929050+bradygaster@users.noreply.github.com>
2026-02-11 08:03:15 -0800

    Create Terrarium.Web with Blazor components (#118)
    * feat: create Terrarium.Web with Blazor components (#22, #24)
    
    * docs: update Skyler history and decisions inbox

--- src/Terrarium.Web/Program.cs
+++ src/Terrarium.Web/Program.cs
@@ -1,13 +1,28 @@
+using Terrarium.Web.Components;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddSignalR();
+
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapGet("/", () => "Terrarium Web");
+app.MapDefaultEndpoints();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
